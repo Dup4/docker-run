c0864324129e35ce89edda71cd7af4bad03df40f
Dup4 <lyuzhi.pan@gmail.com>
2022-10-11 15:55:58 +0800

    feat: redis

---
+++ redis/README.md
@@ -0,0 +1,7 @@
+# redis
+
+## Configuration
+
+You need to go to the redis repo and download the corresponding configuration file.
+
+for example redis:7.0.0, you need to download <https://github.com/redis/redis/blob/7.0/redis.conf> this file specify by tag.
---
+++ redis/start.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+TOP_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
+
+IMAGE_NAME="redis"
+IMAGE_TAG="7.0.0"
+
+docker run \
+    -d \
+    --restart=always \
+    --name="${CONTAINER_NAME}" \
+    --net=host \
+    -v "${TOP_DIR}/data":/data \
+    -v "${TOP_DIR}/conf":/etc/redis \
+    "${IMAGE_NAME}":"${IMAGE_TAG}" \
+    redis-server /etc/redis/redis.conf
